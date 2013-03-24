9ec4d1d64b9b8e19d5031eebe5ca057da936b88e
Mihail Minkov <Mihail.Minkov.BG@gmail.com>
2013-03-24 19:28:24 +0200

    Added script folder.
    The folder contains scripts for building, testing, etc.

--- scripts/frank.sh
+++ scripts/frank.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-USE_SIM_LAUNCHER_SERVER=1
-
 bundle install
 echo 'n' > tmp.txt
 echo '1' >> tmp.txt
 cat tmp.txt | bundle exec frank setup
+
+export USE_SIM_LAUNCHER_SERVER=YES
 bundle exec frank build
 bundle exec cucumber Frank
 FRANK_TEST_IPAD=true bundle exec cucumber Frank
